4e86b92282a8b7a032e4a4ff631f3840244e27ee
Jiahong Li <jack@gofisherman.com>
2020-03-21 20:32:17 -0400

    Initial Commit

---
+++ Weather_Gift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather_Gift
+//
+//  Created by Jia H Li on 3/21/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
