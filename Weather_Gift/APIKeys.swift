e0eeaa49ec501740c582e382dd847f545cfb1d1d
Jiahong Li <jack@gofisherman.com>
2020-03-21 21:34:34 -0400

    with .gitignore

---
+++ Weather_Gift/APIKeys.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeys.swift
+//  Weather_Gift
+//
+//  Created by Jia H Li on 3/21/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyA4R5fXlInriAKxbWO8E_6P2la_-unA94I"
+}
---
+++ Weather_Gift/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  Weather_Gift
+//
+//  Created by Jia H Li on 3/21/20.
+//  Copyright © 2020 Jia H Li. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
